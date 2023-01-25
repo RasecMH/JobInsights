f97f79f3b392aa7a5895ca05b8cfa887fb1bc6ec
RasecMH <rasecmh0@gmail.com>
2023-01-24 21:47:27 -0300

    requisito 12

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    assert list(
+        read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0].keys()
+    ) == ["title", "salary", "type"]
