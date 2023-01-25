aca088b16b88d549da40da05a69895eb47771e32
RasecMH <rasecmh0@gmail.com>
2023-01-24 22:15:15 -0300

    requisito 13

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -20,6 +20,7 @@ from src.flask_app.more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -77,6 +78,13 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs = read(path="data/jobs.csv")
+    ctx = get_job(jobs, index)
+    return render_template("job.jinja2", job=ctx)
 
 
 def init_app(app: Flask):
